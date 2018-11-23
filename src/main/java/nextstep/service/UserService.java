52a939b09db281aa97e00c14e7dd04b484976b02
teachd000000 <teachd000000@gmail.com>
2018-11-23 21:16:03 +0900

    로그인 기능 구현

--- src/main/java/nextstep/service/UserService.java
+++ src/main/java/nextstep/service/UserService.java
@@ -40,6 +40,6 @@ public class UserService {
     public User login(String userId, String password) throws UnAuthenticationException {
         return userRepository.findByUserId(userId)
                 .filter(user -> user.matchPassword(password))
-                .orElseThrow(() -> new UnAuthenticationException());
+                .orElseThrow(UnAuthenticationException::new);
     }
 }
