827998ad1b4e3fbb9ab328723406fa3509c76009
hwanseok-Joo <rokmctkd6@gmail.com>
2018-11-23 21:31:40 +0900

    step1 commit (#21)

--- src/main/java/nextstep/service/UserService.java
+++ src/main/java/nextstep/service/UserService.java
@@ -38,6 +38,10 @@ public class UserService {
 
     public User login(String userId, String password) throws UnAuthenticationException {
         // TODO 로그인 기능 구현
-        return null;
+        //Optional 을 써서 한 라인으로!
+        return userRepository.findByUserId(userId)
+                .filter(user -> user.matchPassword(password))
+                .orElseThrow(UnAuthenticationException::new);
+
     }
 }
