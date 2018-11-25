09be0e7ad6b2e07a940512af37dbccafab3b9143
dotorify <dotorify@gmail.com>
2018-11-25 22:34:57 +0900

    <feature>: QnA 2단계 - 질문 생성 연결

--- src/main/java/nextstep/web/QuestionController.java
+++ src/main/java/nextstep/web/QuestionController.java
@@ -1,6 +1,8 @@
 package nextstep.web;
 
 import nextstep.domain.Question;
+import nextstep.domain.User;
+import nextstep.security.LoginUser;
 import nextstep.service.QnaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
@@ -29,5 +32,11 @@ public class QuestionController {
     public String readDetail(@PathVariable long id, Model model) {
         model.addAttribute("question", qnaService.findById(id).get());
         return "/qna/show";
+    }
+
+    @PostMapping("")
+    public String create(@LoginUser User loginUser, Question question) {
+        qnaService.create(loginUser, question);
+        return "redirect:/";
     }
 }
--- src/test/java/nextstep/web/QuestionAcceptanceTest.java
+++ src/test/java/nextstep/web/QuestionAcceptanceTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.*;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
+
+import java.util.Arrays;
 
 public class QuestionAcceptanceTest extends AcceptanceTest {
     private static final Logger log = LoggerFactory.getLogger(UserAcceptanceTest.class);
@@ -25,11 +29,44 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
     }
 
     @Test
-    public void 질문_상세보기() throws Exception {
+    public void 상세보기() throws Exception {
         ResponseEntity<String> response = template().getForEntity("/questions/" + defaultQuestion().getId(), String.class);
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         log.debug("body : {}", response.getBody());
         softly.assertThat(response.getBody()).contains(defaultQuestion().getTitle());
         softly.assertThat(response.getBody()).contains(defaultQuestion().getContents());
     }
+
+    @Test
+    public void 생성_비로그인_사용자() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("title", "테스트 제목");
+        params.add("contents", "테스트 내용");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = template().postForEntity("/questions", request, String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
+    }
+
+    @Test
+    public void 생성_로그인_사용자() throws Exception {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.TEXT_HTML));
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, Object> params = new LinkedMultiValueMap<>();
+        params.add("title", "테스트 제목");
+        params.add("contents", "테스트 내용");
+        HttpEntity<MultiValueMap<String, Object>> request = new HttpEntity<MultiValueMap<String, Object>>(params, headers);
+
+        ResponseEntity<String> response = basicAuthTemplate().postForEntity("/questions", request, String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+        softly.assertThat(response.getHeaders().getLocation().getPath()).startsWith("/");
+    }
+
+
 }
