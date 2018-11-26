1726b8f1bec0bb92a1d522d8289fa26828a66da8
dotorify <dotorify@gmail.com>
2018-11-27 00:41:58 +0900

    <refactor>: QnA 2단계 - Route 상수화

--- src/main/java/nextstep/web/QuestionController.java
+++ src/main/java/nextstep/web/QuestionController.java
@@ -17,42 +17,47 @@ import javax.annotation.Resource;
 @RequestMapping("/questions")
 public class QuestionController {
     private static final Logger log = LoggerFactory.getLogger(QuestionController.class);
+    public static final String ROUTE_QNA_FORM = "/qna/form";
+    public static final String ROUTE_QNA_SHOW = "/qna/show";
+    public static final String ROUTE_HOME = "redirect:/";
+    public static final String ROUTE_QNA_UPDATE_FORM = "/qna/updateForm";
+    public static final String ATTRIBUTE_QUESTION = "question";
 
     @Resource(name = "qnaService")
     private QnaService qnaService;
 
     @GetMapping("/form")
     public String form() {
-        return "/qna/form";
+        return ROUTE_QNA_FORM;
     }
 
     @GetMapping("/{id}")
     public String readDetail(@PathVariable long id, Model model) {
-        model.addAttribute("question", qnaService.findById(id).get());
-        return "/qna/show";
+        model.addAttribute(ATTRIBUTE_QUESTION, qnaService.findById(id).get());
+        return ROUTE_QNA_SHOW;
     }
 
     @PostMapping("")
     public String create(@LoginUser User loginUser, Question question) {
         qnaService.create(loginUser, question);
-        return "redirect:/";
+        return ROUTE_HOME;
     }
 
     @GetMapping("/{id}/form")
     public String updateForm(@LoginUser User loginUser, @PathVariable long id, Model model) {
-        model.addAttribute("question", qnaService.findById(id).get());
-        return "/qna/updateForm";
+        model.addAttribute(ATTRIBUTE_QUESTION, qnaService.findById(id).get());
+        return ROUTE_QNA_UPDATE_FORM;
     }
 
     @PutMapping("/{id}")
     public String update(@LoginUser User loginUser, @PathVariable long id, Question target) {
         qnaService.update(loginUser, id, target);
-        return "redirect:/";
+        return ROUTE_HOME;
     }
 
     @DeleteMapping("/{id}")
     public String delete(@LoginUser User loginUser, @PathVariable long id) throws CannotDeleteException {
         qnaService.deleteQuestion(loginUser, id);
-        return "redirect:/";
+        return ROUTE_HOME;
     }
 }
