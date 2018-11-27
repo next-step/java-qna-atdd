57fbfb2176eeac87f561b72e2cb88e5b965c991d
Sejong Park <devsejong@gmail.com>
2018-11-27 12:31:05 +0900

    [fix] 2단계 코드리뷰 결과 반영

--- src/main/java/nextstep/domain/QuestionRepository.java
+++ src/main/java/nextstep/domain/QuestionRepository.java
@@ -2,10 +2,6 @@ package nextstep.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface QuestionRepository extends JpaRepository<Question, Long> {
     Iterable<Question> findByDeleted(boolean deleted);
-
-    Optional<Question> findById(Long questionId);
 }
