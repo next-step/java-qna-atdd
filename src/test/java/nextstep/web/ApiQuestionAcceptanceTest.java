a4bcd01917a92cb4ed24e35692558470ea6217d5
dotorify <dotorify@gmail.com>
2018-11-30 01:47:04 +0900

    <test>: QnA 3단계 - 테스트코드 중복 제거

--- src/test/java/nextstep/web/ApiQuestionAcceptanceTest.java
+++ src/test/java/nextstep/web/ApiQuestionAcceptanceTest.java
@@ -1,6 +1,7 @@
 package nextstep.web;
 
 import nextstep.domain.Question;
+import org.junit.Before;
 import org.junit.Test;
 import org.springframework.http.*;
 import support.test.AcceptanceTest;
@@ -8,43 +9,30 @@ import support.test.AcceptanceTest;
 import static nextstep.domain.QuestionTest.newQuestion;
 
 public class ApiQuestionAcceptanceTest extends AcceptanceTest {
+    private Question question;
+    private String location;
+
+    @Before
+    public void setUp() throws Exception {
+        question = newQuestion();
+        location = createResource("/api/questions", question, defaultUser());
+    }
 
     @Test
     public void 생성() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-
-        Question dbQuestion = basicAuthTemplate().getForObject(location, Question.class);
-        softly.assertThat(dbQuestion).isNotNull();
+        softly.assertThat(getResource(location, Question.class)).isNotNull();
     }
 
     @Test
     public void 조회_상세() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        Question question =
-                basicAuthTemplate().getForObject(location, Question.class);
-        softly.assertThat(original.equalsTitleAndContents(question)).isTrue();
+        softly.assertThat(getResource(location, Question.class).equalsTitleAndContents(question)).isTrue();
     }
 
     @Test
     public void 수정() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        Question updateQuestion = new Question(original.getTitle(), "수정된 내용");
-
-        ResponseEntity<Question> responseEntity =
-                basicAuthTemplate().exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), Question.class);
+        Question updateQuestion = new Question("수정된 제목", "수정된 내용");
+        ResponseEntity<Question> responseEntity = basicAuthTemplate()
+                .exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), Question.class);
 
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         softly.assertThat(updateQuestion.equalsTitleAndContents(responseEntity.getBody())).isTrue();
@@ -52,46 +40,27 @@ public class ApiQuestionAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void 수정_비로그인_사용자() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        Question updateQuestion = new Question(original.getTitle(), "수정된 내용");
-
-        ResponseEntity<String> responseEntity =
-                template().exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), String.class);
+        Question updateQuestion = new Question("수정된 제목", "수정된 내용");
+        ResponseEntity<String> responseEntity = template()
+                .exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), String.class);
 
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
     }
 
     @Test
     public void 수정_다른_사용자() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        Question updateQuestion = new Question(original.getTitle(), "수정된 내용");
-
-        ResponseEntity<String> responseEntity =
-                basicAuthTemplate(findByUserId("sanjigi")).exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), String.class);
+        Question updateQuestion = new Question("수정된 제목", "수정된 내용");
+        ResponseEntity<String> responseEntity = basicAuthTemplate(findByUserId("sanjigi"))
+                .exchange(location, HttpMethod.PUT, createHttpEntity(updateQuestion), String.class);
 
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
     }
 
     @Test
     public void 삭제() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        ResponseEntity<Void> responseEntity =
-                basicAuthTemplate().exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
+        Question original = getResource(location, Question.class, defaultUser());
+        ResponseEntity<Void> responseEntity = basicAuthTemplate()
+                .exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
 
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
         softly.assertThat(basicAuthTemplate().getForObject(location, Question.class)).isNull();
@@ -99,21 +68,10 @@ public class ApiQuestionAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void 삭제_다른_사용자() throws Exception {
-        Question newQuestion = newQuestion();
-        ResponseEntity<Void> response = basicAuthTemplate().postForEntity("/api/questions", newQuestion, Void.class);
-        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
-        String location = response.getHeaders().getLocation().getPath();
-        Question original = basicAuthTemplate().getForObject(location, Question.class);
-
-        ResponseEntity<Void> responseEntity =
-                basicAuthTemplate(findByUserId("sanjigi")).exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
+        Question original = getResource(location, Question.class, defaultUser());
+        ResponseEntity<Void> responseEntity = basicAuthTemplate(findByUserId("sanjigi"))
+                .exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
 
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.FORBIDDEN);
-    }
-
-    private HttpEntity createHttpEntity(Object body) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        return new HttpEntity(body, headers);
     }
 }
