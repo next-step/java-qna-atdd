d69bf18701f4535d03c7e97b36034696bf6ab66d
dotorify <dotorify@gmail.com>
2018-11-30 17:46:28 +0900

    <test>: QnA 3단계 - Answer 테스트코드 추가

---
+++ src/test/java/nextstep/domain/AnswerTest.java
@@ -0,0 +1,57 @@
+package nextstep.domain;
+
+import nextstep.CannotDeleteException;
+import nextstep.UnAuthorizedException;
+import org.junit.Before;
+import org.junit.Test;
+import support.test.BaseTest;
+
+public class AnswerTest extends BaseTest {
+    private User originUser;
+    private Answer origin;
+
+
+    @Before
+    public void setUp() throws Exception {
+        originUser = UserTest.JAVAJIGI;
+        origin = newAnswer();
+    }
+
+    @Test(expected = UnAuthorizedException.class)
+    public void 수정_다른작성자() throws Exception {
+        User loginUser = UserTest.SANJIGI;
+        Answer target = new Answer(loginUser, "수정 시도");
+        origin.update(loginUser, target);
+    }
+
+    @Test
+    public void 수정_원글작성자() throws Exception {
+        User loginUser = originUser;
+        Answer target = new Answer(loginUser, "수정 시도");
+        origin.update(loginUser, target);
+        softly.assertThat(origin.getContents()).isEqualTo(target.getContents());
+    }
+
+    @Test(expected = UnAuthorizedException.class)
+    public void 삭제_다른작성자() throws Exception {
+        User loginUser = UserTest.SANJIGI;
+        origin.delete(loginUser);
+    }
+
+    @Test(expected = CannotDeleteException.class)
+    public void 삭제_원답변작성자_지워진_질문() throws Exception {
+        User loginUser = originUser;
+        origin.delete(loginUser);
+        origin.delete(loginUser);
+    }
+
+    @Test
+    public void 삭제_원글작성자() throws Exception {
+        User loginUser = originUser;
+        softly.assertThat(origin.delete(loginUser)).isTrue();
+    }
+
+    public static Answer newAnswer() {
+        return new Answer(UserTest.JAVAJIGI, "새로운 답변");
+    }
+}
--- src/test/java/nextstep/web/ApiAnswerAcceptanceTest.java
+++ src/test/java/nextstep/web/ApiAnswerAcceptanceTest.java
@@ -2,41 +2,43 @@ package nextstep.web;
 
 import nextstep.domain.Answer;
 import nextstep.domain.Question;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import support.test.AcceptanceTest;
 
+import javax.xml.ws.Response;
+
 public class ApiAnswerAcceptanceTest extends AcceptanceTest {
-    @Test
-    public void 생성() throws Exception {
+    private String location;
+
+    @Before
+    public void setUp() throws Exception {
         Question question = defaultQuestion();
         Answer answer = new Answer(defaultUser(), "내용");
-        String location = createResource("/api/questions/" + question.getId() + "/answers", answer, defaultUser());
+        location = createResource("/api/questions/" + question.getId() + "/answers", answer, defaultUser());
+    }
+
+    @Test
+    public void 생성() throws Exception {
         softly.assertThat(getResource(location, Question.class)).isNotNull();
     }
 
     @Test
     public void 수정() {
-        Question question = defaultQuestion();
-        Answer answer = new Answer(defaultUser(), "내용");
-        String location = createResource("/api/questions/" + question.getId() + "/answers", answer, defaultUser());
-        Answer updateAnswer = new Answer(defaultUser(), "수정된 내용");
-
+        String contents = "수정된 내용";
         ResponseEntity<Answer> responseEntity = basicAuthTemplate()
-                .exchange(location, HttpMethod.PUT, createHttpEntity(updateAnswer), Answer.class);
+                .exchange(location, HttpMethod.PUT, createHttpEntity(new Answer(defaultUser(), contents)), Answer.class);
         softly.assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
-        softly.assertThat(getResource(location, Answer.class, defaultUser()).getContents()).isEqualTo(updateAnswer.getContents());
+        softly.assertThat(getResource(location, Answer.class, defaultUser()).getContents()).isEqualTo(contents);
     }
 
     @Test
     public void 삭제() {
-        Question question = defaultQuestion();
-        Answer answer = new Answer(defaultUser(), "내용");
-        String location = createResource("/api/questions/" + question.getId() + "/answers", answer, defaultUser());
         Answer original = getResource(location, Answer.class, defaultUser());
-
         ResponseEntity<Void> responseEntity = basicAuthTemplate()
                 .exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
 
@@ -46,11 +48,7 @@ public class ApiAnswerAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void 삭제_다른_사용자() {
-        Question question = defaultQuestion();
-        Answer answer = new Answer(defaultUser(), "내용");
-        String location = createResource("/api/questions/" + question.getId() + "/answers", answer, defaultUser());
         Answer original = getResource(location, Answer.class, defaultUser());
-
         ResponseEntity<Void> responseEntity = basicAuthTemplate(findByUserId("sanjigi"))
                 .exchange(location, HttpMethod.DELETE, createHttpEntity(original.getId()), Void.class);
 
