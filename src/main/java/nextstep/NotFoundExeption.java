c5346b676aa4da513433cca920b007f17c820eaa
serena <noriming2@gmail.com>
2018-11-30 14:21:40 +0900

    add NotFoundException

--- src/main/java/nextstep/NotFoundExeption.java
+++ src/main/java/nextstep/NotFoundExeption.java
@@ -1,6 +1,6 @@
 package nextstep;
 
-public class NotFoundExeption extends Exception {
+public class NotFoundExeption extends RuntimeException { //사용자의 잘못 된 사용은 Runtime으로 처리하는 것이 맞음.
     private static final long serialVersionUID = 1L;
 
     public NotFoundExeption() {
