155f1b890e07590c7c4659d0cc8e1727cd24c631
Park Dongwook <wanderhoward@gmail.com>
2019-03-30 14:42:14 +0900

    Step 2. QnaController - list() 작성

--- src/test/java/nextstep/web/QnaAcceptanceTest.java
+++ src/test/java/nextstep/web/QnaAcceptanceTest.java
@@ -1,5 +1,6 @@
 package nextstep.web;
 
+import nextstep.domain.Question;
 import nextstep.domain.QuestionRepository;
 import nextstep.domain.User;
 import org.junit.Test;
@@ -21,7 +22,11 @@ public class QnaAcceptanceTest extends AcceptanceTest {
 
   @Test
   public void createForm() throws Exception {
+
+    // When
     ResponseEntity<String> response = template().getForEntity("/qnas/form", String.class);
+
+    // Then
     softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     log.debug("body : {}", response.getBody());
   }
@@ -29,15 +34,35 @@ public class QnaAcceptanceTest extends AcceptanceTest {
   @Test
   public void create() throws Exception {
 
+    // Given
     User loginUser = defaultUser();
     HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
         .addParameter("title", "질문 제목")
         .addParameter("contents", "질문 내용")
         .build();
 
+    // When
     ResponseEntity<String> response = basicAuthTemplate(loginUser).postForEntity("/qnas", request, String.class);
 
+    // Then
     softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
     softly.assertThat(response.getHeaders().getLocation().getPath()).isEqualTo("/");
+  }
+
+  @Test
+  public void list() throws Exception {
+
+    // When
+    ResponseEntity<String> response = template().getForEntity("/", String.class);
+
+    // Then
+    softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+    log.debug("body : {}", response.getBody());
+
+    Question question = questionRepository.findById(1L)
+        .orElseThrow(NullPointerException::new);
+
+    softly.assertThat(response.getBody()).contains(question.generateUrl());
+    softly.assertThat(response.getBody()).contains(question.getTitle());
   }
 }
