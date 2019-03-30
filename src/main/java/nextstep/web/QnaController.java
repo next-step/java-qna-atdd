54e9a64aafe245c9ab3ef8a36bbd1a76a4a3120b
Park Dongwook <wanderhoward@gmail.com>
2019-03-30 14:32:40 +0900

    Step 2. QnaController - create() 작성

--- src/main/java/nextstep/web/QnaController.java
+++ src/main/java/nextstep/web/QnaController.java
@@ -2,11 +2,15 @@ package nextstep.web;
 
 
 import javax.annotation.Resource;
+import nextstep.domain.Question;
+import nextstep.domain.User;
+import nextstep.security.LoginUser;
 import nextstep.service.QnaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,5 +25,11 @@ public class QnaController {
   @GetMapping("/form")
   public String form() {
     return "/qna/form";
+  }
+
+  @PostMapping("")
+  public String create(@LoginUser User loginUser, Question question) {
+    qnaService.create(loginUser, question);
+    return "redirect:/";
   }
 }
--- src/test/java/nextstep/web/QnaAcceptanceTest.java
+++ src/test/java/nextstep/web/QnaAcceptanceTest.java
@@ -1,12 +1,15 @@
 package nextstep.web;
 
 import nextstep.domain.QuestionRepository;
+import nextstep.domain.User;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import support.test.AcceptanceTest;
 
 public class QnaAcceptanceTest extends AcceptanceTest {
@@ -21,5 +24,20 @@ public class QnaAcceptanceTest extends AcceptanceTest {
     ResponseEntity<String> response = template().getForEntity("/qnas/form", String.class);
     softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
     log.debug("body : {}", response.getBody());
+  }
+
+  @Test
+  public void create() throws Exception {
+
+    User loginUser = defaultUser();
+    HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
+        .addParameter("title", "질문 제목")
+        .addParameter("contents", "질문 내용")
+        .build();
+
+    ResponseEntity<String> response = basicAuthTemplate(loginUser).postForEntity("/qnas", request, String.class);
+
+    softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.FOUND);
+    softly.assertThat(response.getHeaders().getLocation().getPath()).isEqualTo("/");
   }
 }
