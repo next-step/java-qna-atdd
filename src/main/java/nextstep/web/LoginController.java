678509161154989786d232edc4977a8695c088d2
devson <sonyc5720@gmail.com>
2019-03-31 16:06:49 +0900

    로그인 기능 구현

---
+++ src/main/java/nextstep/web/LoginController.java
@@ -0,0 +1,39 @@
+package nextstep.web;
+
+import nextstep.UnAuthenticationException;
+import nextstep.domain.User;
+import nextstep.security.HttpSessionUtils;
+import nextstep.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    private final UserService userService;
+
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String loginView() {
+        return "/user/login";
+    }
+
+    @PostMapping
+    public String login(HttpSession session, User user) {
+        try {
+            userService.login(user.getUserId(), user.getPassword());
+            HttpSessionUtils.login(session, user);
+        } catch (UnAuthenticationException e) {
+            return "/user/login_failed";
+        }
+
+        return "redirect:/users";
+    }
+}
