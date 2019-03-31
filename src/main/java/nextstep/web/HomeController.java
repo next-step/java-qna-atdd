6e9dbbd580425dcf3569b63150423390e15ff47d
hyoj <chohyojoy@gmail.com>
2019-03-31 11:22:37 +0900

    Step2 로그아웃 ATDD 기반 구현

--- src/main/java/nextstep/web/HomeController.java
+++ src/main/java/nextstep/web/HomeController.java
@@ -1,5 +1,8 @@
 package nextstep.web;
 
+import javax.servlet.http.HttpSession;
+import nextstep.domain.User;
+import nextstep.security.HttpSessionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,5 +12,18 @@ public class HomeController {
     @GetMapping("/")
     public String home(Model model) {
         return "home";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        User user = HttpSessionUtils.getUserFromSession(httpSession);
+
+        if (user.isGuestUser()) {
+            throw new IllegalAccessError();
+        }
+
+        httpSession.removeAttribute(HttpSessionUtils.USER_SESSION_KEY);
+
+        return "redirect:/";
     }
 }
