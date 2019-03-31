ed3d71f27013862dc213a811861697d7a02952ec
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-03-31 22:36:44 +0900

    # 실패 test case 수정 - 테스트 데이터 분리 ( 테스트별로 별도의 Question 데이터 사용하도록 setUp method 추가 )
    * 같은 테스트 데이터를 사용하다 보니, 삭제 테스트 이후에는 데이터를 찾지못해 test fail이 발생함

--- src/test/java/nextstep/web/QuestionAcceptanceTest.java
+++ src/test/java/nextstep/web/QuestionAcceptanceTest.java
@@ -1,8 +1,11 @@
 package nextstep.web;
 
+import nextstep.domain.Question;
 import nextstep.domain.QuestionRepository;
 import nextstep.domain.User;
 import nextstep.web.lib.HtmlFormDataBuilder;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,14 +24,34 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
     //TODO : Read -> Create -> Delete -> Update 순서로 진행
 
     //1. 목록보기 (get) : /qna
+        //TODO : 삭제된 목록은 제외하고 출력
     //2. 상세보기 (get) : /qna/{id}
 
     //3. 등록하기 (post) : /qna
     //4. 삭제하기 (delete) : /qna/{id}
+        //TODO : 실제 삭제하는게 아니고 deleted field를 true로 변경
+
     //5. 수정하기 (put) : /qna/{id}
 
     @Autowired
     private QuestionRepository questionRepository;
+
+    private static long testQuestionId;
+
+    @Before
+    public void setUp() throws Exception {
+        Question question = new Question("title", "contents");
+        question.writeBy(defaultUser());
+
+        this.testQuestionId = questionRepository.save(question).getId();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        if (questionRepository.existsById(testQuestionId)) {
+            questionRepository.deleteById(this.testQuestionId);
+        }
+    }
 
     @Test
     public void home_qnaList() throws Exception {
@@ -104,7 +127,7 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
         HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
                 .delete()
                 .build();
-        return template.postForEntity(String.format("/questions/%d", defaultQuestion().getId()), request, String.class);
+        return template.postForEntity(String.format("/questions/%d", testQuestionId), request, String.class);
     }
 
     @Test
@@ -121,25 +144,25 @@ public class QuestionAcceptanceTest extends AcceptanceTest {
 
     @Test
     public void update_no_login() {
-        ResponseEntity<String> response = update2(template());
+        ResponseEntity<String> response = update(template());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.UNAUTHORIZED);
         log.debug("body : {}", response.getBody());
     }
 
     @Test
     public void update_login() {
-        ResponseEntity<String> response = update2(basicAuthTemplate());
+        ResponseEntity<String> response = update(basicAuthTemplate());
         softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         log.debug("body : {}", response.getBody());
     }
 
-    private ResponseEntity<String> update2(TestRestTemplate template) {
+    private ResponseEntity<String> update(TestRestTemplate template) {
         HttpEntity<MultiValueMap<String, Object>> request = HtmlFormDataBuilder.urlEncodedForm()
                 .put()
                 .addParameter("title", "제목수정")
                 .addParameter("contents", "내용도 수정")
                 .build();
 
-        return template.postForEntity(String.format("/questions/%d", defaultQuestion().getId()), request, String.class);
+        return template.postForEntity(String.format("/questions/%d", testQuestionId), request, String.class);
     }
 }
