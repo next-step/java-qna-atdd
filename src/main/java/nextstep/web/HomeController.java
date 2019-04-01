189ea497b81d17a8fe583c7ff1062b8e86bd094b
JoonT92 <pk920207@gmail.com>
2019-04-02 02:32:14 +0900

    feat : Home 구현
    - HomeAcceptanceTest 구현
    - HomeController 구현
    - QnaService list 테스트

--- src/main/java/nextstep/web/HomeController.java
+++ src/main/java/nextstep/web/HomeController.java
@@ -1,13 +1,26 @@
 package nextstep.web;
 
+import nextstep.domain.Question;
+import nextstep.service.QnaService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
 public class HomeController {
+    private final QnaService qnaService;
+
+    public HomeController(QnaService qnaService) {
+        this.qnaService = qnaService;
+    }
+
     @GetMapping("/")
     public String home(Model model) {
+        List<Question> list = qnaService.findAll();
+
+        model.addAttribute("questions", list);
         return "home";
     }
 }
---
+++ src/test/java/nextstep/web/HomeAcceptanceTest.java
@@ -0,0 +1,47 @@
+package nextstep.web;
+
+import nextstep.domain.Question;
+import nextstep.domain.QuestionRepository;
+import nextstep.domain.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import support.test.AcceptanceTest;
+
+public class HomeAcceptanceTest extends AcceptanceTest {
+    private static final Logger log = LoggerFactory.getLogger(HomeAcceptanceTest.class);
+
+    @Autowired private QuestionRepository questionRepository;
+
+    @Test
+    public void 질문목록이_출력된다() {
+        questionRepository.save(new Question("This is title1", "This is contents1"));
+        questionRepository.save(new Question("This is title2", "This is contents2"));
+
+        ResponseEntity<String> response = template().getForEntity("/", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        softly.assertThat(response.getBody()).contains("This is title1");
+        softly.assertThat(response.getBody()).contains("This is title2");
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void 로그인하면_질문하기가_출력된다() {
+        ResponseEntity<String> response = basicAuthTemplate().getForEntity("/", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        softly.assertThat(response.getBody()).contains("질문하기");
+        log.debug("body : {}", response.getBody());
+    }
+
+    @Test
+    public void 로그인하지_않으면_질문하기가_출력되지_않는다() {
+        ResponseEntity<String> response = template().getForEntity("/", String.class);
+        softly.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+        softly.assertThat(response.getBody()).doesNotContain("질문하기");
+        log.debug("body : {}", response.getBody());
+    }
+}
