eebc509421c5f11bee6c0108dc68e749d0fddc0f
SGKIM94 <tkdrn8578@naver.com>
2019-04-03 22:41:58 +0900

    QuestionController  * 질문의 CRUD를 구성하는 Controller 구현

---
+++ src/main/java/nextstep/web/QuestionController.java
@@ -0,0 +1,67 @@
+package nextstep.web;
+
+import nextstep.CannotDeleteException;
+import nextstep.domain.Question;
+import nextstep.domain.User;
+import nextstep.security.LoginUser;
+import nextstep.service.QnaService;
+import nextstep.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Controller
+@RequestMapping("/questions")
+public class QuestionController {
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+    @Resource(name = "qnaService")
+    private QnaService qnaService;
+
+    @Resource(name = "userService")
+    private UserService userService;
+
+    @GetMapping("/form")
+    public String form() {
+        return "/qna/form";
+    }
+
+    @PostMapping("")
+    public String create(@LoginUser User user, Question question) {
+        qnaService.create(user, question);
+        return "redirect:/questions/form";
+    }
+
+    @GetMapping("/list")
+    public String list(Model model, Pageable pageable) {
+        List<Question> questions = qnaService.findAll(pageable);
+        log.debug("question size : {}", questions.size());
+
+        model.addAttribute("questions", questions);
+        return "/home";
+    }
+
+    @GetMapping("/{id}/form")
+    public String updateForm(@PathVariable long id, Model model) {
+        model.addAttribute("question", qnaService.findById(id));
+        return "/qna/show";
+    }
+
+    @PutMapping("/{id}")
+    public String update(@LoginUser User loginUser, @PathVariable long id, Question target) {
+        qnaService.update(loginUser, id, target);
+        return "redirect:/questions";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@LoginUser User loginUser, @PathVariable long id) throws CannotDeleteException {
+        qnaService.deleteQuestion(loginUser, id);
+        return "redirect:/questions";
+    }
+}
