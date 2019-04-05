5577b7cedde26941226544bd688c276f5775a11a
devson <sonyc5720@gmail.com>
2019-04-05 13:08:52 +0900

    client에서 RestAPI로 잘못된 parameter를 보냈을 때 발생하는 IllegalArgument를 처리하기 위한 ControllerAdvice

---
+++ src/main/java/nextstep/web/RestApiControllerAdvice.java
@@ -0,0 +1,20 @@
+package nextstep.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(annotations = RestController.class)
+public class RestApiControllerAdvice {
+    private static final Logger log = LoggerFactory.getLogger(RestApiControllerAdvice.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public void illegalArgumentException(IllegalArgumentException e) {
+        log.debug("JSON API IllegalArgumentException is happened!");
+    }
+}
