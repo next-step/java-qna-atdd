f5cd5298fce51056d8e6bfd61224a8fce5753a34
isisrin <isisrin@nate.com>
2019-04-05 20:40:44 +0900

    step2 fixture 수정

--- src/test/java/nextstep/domain/Fixture.java
+++ src/test/java/nextstep/domain/Fixture.java
@@ -5,5 +5,5 @@ public class Fixture {
     public static String contents = "뚜루루뚜루";
     public static Question mockQuestion = new Question(title, contents);
     public static User mockUser = new User("sanjigi", "test", "산지기", "sanjigi@slipp.net");
-    public static Answer answer = new Answer(mockUser, "껀톈뚜");
+    public static Answer answer = new Answer(1L, mockUser, mockQuestion, "껀톈뚜");
 }
