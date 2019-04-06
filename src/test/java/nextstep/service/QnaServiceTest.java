51540aceeebda3cf7f2cc24300a27c2d4b4ff64d
isisrin <isisrin@nate.com>
2019-04-06 23:29:08 +0900

    step3 피드백반영 중... 의심가는 애들 커밋  - QnaServiceTest추가

--- src/test/java/nextstep/service/QnaServiceTest.java
+++ src/test/java/nextstep/service/QnaServiceTest.java
@@ -6,28 +6,27 @@ import nextstep.domain.*;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import support.test.BaseTest;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 
-import static nextstep.domain.Fixture.mockQuestion;
-import static nextstep.domain.Fixture.mockUser;
-import static org.mockito.Mockito.*;
+import static nextstep.domain.Fixture.*;
 
-@RunWith(MockitoJUnitRunner.Silent.class)
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class QnaServiceTest extends BaseTest {
 
-    @Mock
+    @Autowired
     private QuestionRepository questionRepository;
 
-    @Mock
+    @Autowired
     private AnswerRepository answerRepository;
 
-    @InjectMocks
+    @Autowired
     private QnaService qnaService;
 
     private Optional<Question> returnCacheValue;
@@ -35,26 +34,22 @@ public class QnaServiceTest extends BaseTest {
 
     @Before
     public void setUp() {
-        mockQuestion.writeBy(mockUser);
-        Fixture.answer.setId(1);
-        returnCacheValue = Optional.of((Question) mockQuestion);
-        returnCacheAnswer = Optional.of((Answer) Fixture.answer);
+        MOCK_QUESTION.writeBy(MOCK_USER);
+        ANSWER.setId(1);
+        returnCacheValue = Optional.of((Question) MOCK_QUESTION);
+        returnCacheAnswer = Optional.of((Answer) ANSWER);
     }
 
     @Test
     public void 생성테스트() {
-        when(questionRepository.save(mockQuestion)).thenReturn(mockQuestion);
-
-        Question result = qnaService.create(mockUser, mockQuestion);
-        softly.assertThat(result).isEqualTo(mockQuestion);
+        Question result = qnaService.create(MOCK_USER, MOCK_QUESTION);
+        softly.assertThat(result).isEqualTo(MOCK_QUESTION);
         softly.assertThat(result.getWriter()).isNotNull();
     }
 
     @Test
     public void 업데이트_테스트() throws Exception {
-        when(questionRepository.findById(new Long(2))).thenReturn(returnCacheValue);
-
-        Question result = qnaService.update(new User("sanjigi", "password", "산지기", "sanjigi@slipp.net"), 2, new Question("아기상어", "아빠상어"));
+        Question result = qnaService.update(new User("sanjigi", "password", "산지기", "sanjigi@slipp.net"), 2, Question.builder().title("아기상어").contents("아빠상어").build());
         softly.assertThat(result.getTitle()).isEqualTo("아기상어");
         softly.assertThat(result.getContents()).isEqualTo("아빠상어");
         softly.assertThat(result.getWriter()).isNotNull();
@@ -62,74 +57,56 @@ public class QnaServiceTest extends BaseTest {
 
     @Test(expected = UnAuthenticationException.class)
     public void 업데이트_실패_테스트() throws Exception {
-        Question test = mockQuestion;
-        test.writeBy(mockUser);
-        returnCacheValue = Optional.of((Question) test);
-        when(questionRepository.findById(new Long(1))).thenReturn(returnCacheValue);
-
-        Question result = qnaService.update(new User("이상하네", "아무리봐도", "다른데", "왜그래@slipp.net"), 1, mockQuestion);
+        Question result = qnaService.update(new User("이상하네", "아무리봐도", "다른데", "왜그래@slipp.net"), 1, MOCK_QUESTION);
         softly.assertThat(result.getContents()).isEqualTo("뚜루루뚜루");
         softly.assertThat(result.getTitle()).isEqualTo("아기상어");
         softly.assertThat(result.getWriter()).isNotNull();
     }
 
 
+    // TODO : 이거 결과값 체크 필요
     @Test
     public void 삭제_테스트() throws CannotDeleteException {
-        when(questionRepository.findById(new Long(2))).thenReturn(returnCacheValue);
-        qnaService.deleteQuestion(new User("sanjigi", "test", "산지기", "sanjigi@slipp.net"), 2);
+        qnaService.deleteQuestion(OTHER_USER, 2);
     }
 
     @Test(expected = EntityNotFoundException.class)
     public void 삭제_실패_테스트() throws CannotDeleteException {
-        when(questionRepository.findById(new Long(0))).thenReturn(Optional.empty());
-
         qnaService.deleteQuestion(new User("sanjigi2", "password", "name", "javajigi@slipp.net"), 0);
     }
 
     @Test
     public void 답변_추가_테스트() {
-        when(questionRepository.findById(new Long(0))).thenReturn(returnCacheValue);
-
-        Answer result = qnaService.addAnswer(new User("sanjigi", "password", "name", "javajigi@slipp.net"), 0, "엄마상어는요!");
+        Answer result = qnaService.addAnswer(MOCK_USER, 1, "엄마상어는요!");
         softly.assertThat(result.getContents()).isEqualTo("엄마상어는요!");
     }
 
     @Test
     public void 답변_수정_테스트() throws Exception {
-        when(answerRepository.findById(new Long(1))).thenReturn(returnCacheAnswer);
-
-        Answer result = qnaService.updateAnswer(mockUser, 1L, "여기까지가~~ 끝인가바여!");
+        Answer result = qnaService.updateAnswer(MOCK_USER, 1L, "여기까지가~~ 끝인가바여!");
         softly.assertThat(result.getContents()).isEqualTo("여기까지가~~ 끝인가바여!");
     }
 
     @Test(expected = EntityNotFoundException.class)
     public void 답변_수정_실패테스트_없는것삭제() throws Exception {
-        when(answerRepository.findById(new Long(1))).thenReturn(returnCacheAnswer);
-
-        Answer result = qnaService.updateAnswer(mockUser, 7L, "여기까지가~~ 끝인가바여!");
+        qnaService.updateAnswer(MOCK_USER, 7L, "여기까지가~~ 끝인가바여!");
     }
 
     @Test(expected = UnAuthenticationException.class)
     public void 답변_수정_실패테스트_유저다름() throws Exception {
-        when(answerRepository.findById(1L)).thenReturn(returnCacheAnswer);
-        Answer result = qnaService.updateAnswer(new User("hyerin", "password", "hyerin", "hyerin@hyerin.net"), 1L, "여기까지가~~ 끝인가바여!");
+        qnaService.updateAnswer(new User("hyerin", "password", "hyerin", "hyerin@hyerin.net"), 1L, "여기까지가~~ 끝인가바여!");
     }
 
     @Test
-    public void 답변_삭제_테스트() throws CannotDeleteException {
-        when(answerRepository.findById(new Long(0))).thenReturn(returnCacheAnswer);
-        doNothing().when(answerRepository).delete(Fixture.answer);
+    public void 답변_삭제_테스트() throws Exception {
+        qnaService.deleteAnswer(MOCK_USER, 1);
 
-        qnaService.deleteAnswer(mockUser, 0);
-        
-        verify(answerRepository, times(1)).delete(Fixture.answer);
+        boolean result = answerRepository.findById(1L).orElse(null).isDeleted();
+        softly.assertThat(result).isEqualTo(true);
     }
 
     @Test(expected = EntityNotFoundException.class)
-    public void 답변_삭제_실패_테스트() throws EntityNotFoundException {
-        when(answerRepository.findById(new Long(0))).thenReturn(returnCacheAnswer);
-
-        qnaService.deleteAnswer(new User("sanjigi2", "password", "name", "javajigi@slipp.net"), 1);
+    public void 답변_삭제_실패_테스트() throws Exception {
+        qnaService.deleteAnswer(MOCK_USER, 0);
     }
 }
