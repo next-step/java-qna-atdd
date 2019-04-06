7e9f4f246bed5ee2c5e7b7fe6fdfc54895f5b666
willing1026 <34908960+willing1026@users.noreply.github.com>
2019-04-06 16:51:03 +0900

    # Answer Entity update, delete 메서드 추가 및 테스트

--- src/main/java/nextstep/domain/Answer.java
+++ src/main/java/nextstep/domain/Answer.java
@@ -1,5 +1,6 @@
 package nextstep.domain;
 
+import nextstep.UnAuthorizedException;
 import support.domain.AbstractEntity;
 import support.domain.UrlGeneratable;
 
@@ -65,6 +66,23 @@ public class Answer extends AbstractEntity implements UrlGeneratable {
 
     public boolean isDeleted() {
         return deleted;
+    }
+
+    public Answer update(User loginUser, Answer newAnswer) {
+        if (!isOwner(loginUser)) {
+            throw new UnAuthorizedException();
+        }
+
+        this.contents = newAnswer.contents;
+        return this;
+    }
+
+    public Answer delete(User loginUser) {
+        if (!isOwner(loginUser)) {
+            throw new UnAuthorizedException();
+        }
+        this.deleted = Boolean.TRUE;
+        return this;
     }
 
     @Override
---
+++ src/test/java/nextstep/domain/AnswerTest.java
@@ -0,0 +1,74 @@
+package nextstep.domain;
+
+import nextstep.UnAuthorizedException;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnswerTest {
+
+    private User answerWriter;
+    private User questionWriter;
+    private Question question;
+
+    @Before
+    public void setUp() throws Exception {
+        answerWriter = new User(1, "userId","password","name,", "gmail@mail.com");
+        questionWriter = new User(2, "guest","password","guest,", "gmail@mail.com");
+        question = new Question("title", "content");
+    }
+
+    @Test
+    public void create() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        assertThat(answer).isNotNull();
+    }
+
+    @Test
+    public void mapping_question() {
+        Answer answer = new Answer(answerWriter, "답변");
+        answer.toQuestion(question);
+        assertThat(answer).isNotNull();
+    }
+
+    //onwer check
+    @Test
+    public void isOnwer() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        assertThat(answer.isOwner(questionWriter)).isEqualTo(false);
+    }
+
+    //update
+    @Test
+    public void update() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        Answer newAnswer = new Answer(answerWriter, "답변");
+        answer.update(answerWriter, newAnswer);
+
+        assertThat(answer.getContents()).isEqualTo(newAnswer.getContents());
+    }
+
+    @Test(expected = UnAuthorizedException.class)
+    public void update_다른사용자() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        Answer newAnswer = new Answer(answerWriter, "답변");
+
+        answer.update(questionWriter, newAnswer);
+    }
+
+    //delete
+    @Test
+    public void delete() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        answer.delete(answerWriter);
+        assertThat(answer.isDeleted()).isTrue();
+    }
+
+    @Test(expected = UnAuthorizedException.class)
+    public void delete_다른사용자() {
+        Answer answer = new Answer(1L, answerWriter, question, "답변");
+        answer.delete(questionWriter);
+        assertThat(answer.isDeleted()).isTrue();
+    }
+}
