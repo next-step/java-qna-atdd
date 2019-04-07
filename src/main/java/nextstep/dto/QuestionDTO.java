cdece543400280e835579f497dbbc0fb195de9f1
anjaemin <woalse1014@gmail.com>
2019-04-07 17:28:12 +0900

    step3 구현 - ApiQnaController 추가 및 구현 - ApiQnaAcceptanceTest 추가 - QuestionDTO 수정 - Question domain @JsonIdentityInfo 추가

--- src/main/java/nextstep/dto/QuestionDTO.java
+++ src/main/java/nextstep/dto/QuestionDTO.java
@@ -3,11 +3,21 @@ package nextstep.dto;
 import nextstep.domain.Question;
 
 public class QuestionDTO {
-    private final Question question;
-    private final int answerSize;
+    private Question question;
+    private int answerSize;
+
+    public QuestionDTO() {}
 
     public QuestionDTO(Question question, int answerSize) {
         this.question = question;
+        this.answerSize = answerSize;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public void setAnswerSize(int answerSize) {
         this.answerSize = answerSize;
     }
 
